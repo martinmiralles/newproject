b4e46d4fcb37953082ffed6c9da77067ebd41ed7
Martin Miralles <martinmiralles66@gmail.com>
2020-01-12 18:38:47 -0500

    Added React static files

---
+++ src/main/resources/static/precache-manifest.cd9ab5aa04fce8a8f3ba94a6d4888467.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f4e750fadfacf21407e990247bf415",
+    "url": "/index.html"
+  },
+  {
+    "revision": "226d9e9d8ed39bbd58e2",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "853a58951bbeaad73cb8",
+    "url": "/static/css/main.a7dc2f72.chunk.css"
+  },
+  {
+    "revision": "226d9e9d8ed39bbd58e2",
+    "url": "/static/js/2.b6622225.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.b6622225.chunk.js.LICENSE"
+  },
+  {
+    "revision": "853a58951bbeaad73cb8",
+    "url": "/static/js/main.a71a5e19.chunk.js"
+  },
+  {
+    "revision": "0e0de284a6b544b8ab0b",
+    "url": "/static/js/runtime-main.df9c3e05.js"
+  }
+]);
